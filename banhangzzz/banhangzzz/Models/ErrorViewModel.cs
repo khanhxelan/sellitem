142307c1cee772b638270c8ea4967c3732e040ce
Administrator <Administrator@PMT2017>
2026-06-19 14:57:42 +0700

    lan 2

---
+++ banhangzzz/banhangzzz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace banhangzzz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
